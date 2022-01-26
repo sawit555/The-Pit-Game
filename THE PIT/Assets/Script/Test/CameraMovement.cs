fb58ae6ba1972f725722ec9d9fbaf540df808087
CipherZ <maxza1970@gmail.com>
2022-01-26 10:24:00 +0700

    Update 1/26/2022 Home

--- THE PIT/Assets/Script/Test/CameraMovement.cs
+++ THE PIT/Assets/Script/Test/CameraMovement.cs
@@ -23,12 +23,12 @@ public class CameraMovement : MonoBehaviour
             {
                 Vector3 targetPosition = new Vector3(target.position.x, target.position.y, transform.position.z);
 
-                //targetPosition.x = Mathf.Clamp(targetPosition.x,
-                //                                minPosition.x,
-                //                                maxPosition.x);
-                //targetPosition.y = Mathf.Clamp(targetPosition.y,
-                //                                minPosition.y,
-                //                                maxPosition.y);
+                targetPosition.x = Mathf.Clamp(targetPosition.x,
+                                                minPosition.x,
+                                                maxPosition.x);
+                targetPosition.y = Mathf.Clamp(targetPosition.y,
+                                                minPosition.y,
+                                                maxPosition.y);
                 transform.position = Vector3.Lerp(transform.position, targetPosition, smoothing);
             }
         }
